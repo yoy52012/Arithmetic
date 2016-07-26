29b7025d3e692faa2133d31c780152d063833db3
yuanchao <yuanchao52012@163.com>
2016-07-26 09:47:38 +0800

    一些新的算法程序

---
+++ Odd and Even Exchange(奇偶交换).java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Exchange {
+    public int exchangeOddEven(int x) {
+        // write code here
+        return ((x&0xAAAAAAAA)>>1)|((x&0x55555555)<<1);
+    }
+}
